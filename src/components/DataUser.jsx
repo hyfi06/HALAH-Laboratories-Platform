f6426e64b787212282c6813596d0baef9446323b
Humberto Lopez <humbertolopez954@gmail.com>
2020-05-20 02:15:26 -0500

    [Add] Add component UserData

---
+++ src/components/DataUser.jsx
@@ -0,0 +1,6 @@
+import '../assets/styles/components/DataUser.scss';
+
+function DataUser() {
+  return <h1> Hola mundo</h1>;
+}
+export default DataUser;
---
+++ src/pages/profile.jsx
@@ -0,0 +1,14 @@
+import '../assets/styles/pages/profile.scss';
+import DataUser from '../components/DataUser';
+import User from '../components/User';
+
+function Profile() {
+  return (
+    <>
+      <User />
+      <DataUser />
+    </>
+  );
+}
+
+export default Profile;
