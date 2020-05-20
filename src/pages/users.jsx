2f4dd269ad439e83f725e608e20b304380737fa4
alberthor47 <albertocamarena.77t@gmail.com>
2020-05-19 23:27:15 -0500

    [ADD] new component Table | [READY] users page

--- src/pages/users.jsx
+++ src/pages/users.jsx
@@ -1,11 +1,22 @@
+import PropTypes from 'prop-types';
 import Filter from '../components/Filter';
+import Table from '../components/Table';
 
-function Users() {
+function Users({ userType }) {
   return (
     <>
-      <Filter />
+      <Filter typeOfUser={userType} />
+      <Table />
     </>
   );
 }
+
+Users.propTypes = {
+  userType: PropTypes.string,
+};
+
+Users.defaultProps = {
+  userType: 'administrator',
+};
 
 export default Users;
