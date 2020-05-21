23d9b977c082ee1f160c71f9c463ce3bb77f39e6
alberthor47 <albertocamarena.77t@gmail.com>
2020-05-21 13:19:35 -0500

    [STYLES] fixing linter style & confirmation button

--- src/components/UserDelete.jsx
+++ src/components/UserDelete.jsx
@@ -7,7 +7,7 @@ function UserDelete() {
       <WarningIcon className="user-delete__icon" />
       <h2 className="user-delete__text">Are you sure you want to delete this user?</h2>
       <div className="user-delete__buttons">
-        <button className="btn--positive" type="submit">Accept</button>
+        <button className="btn--positive" type="submit">Confirm</button>
         <button className="btn--negative" type="submit">Cancel</button>
       </div>
     </div>
