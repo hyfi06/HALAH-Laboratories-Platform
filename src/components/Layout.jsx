9f3f63ba66a2ed592ecb3d71724d50456934b733
Luis Flores Álvarez <luisfloresdev@gmail.com>
2020-05-21 22:23:01 -0500

    [ADD] Created session conntext

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,33 +1,14 @@
-import { useState, useEffect } from 'react';
-import Router from 'next/router';
 import PropTypes from 'prop-types';
 import '../assets/styles/components/Layout.scss';
 import Header from './Header';
 import Navbar from './Navbar';
 
 function Layout({ children }) {
-  const [session, setSession] = useState({});
-
-  useEffect(() => {
-    try {
-      setSession(
-        JSON.parse(
-          document.cookie.replace(
-            /(?:(?:^|.*;\s*)session\s*\=\s*([^;]*).*$)|^.*$/,
-            '$1',
-          ),
-        ),
-      );
-    } catch (error) {
-      Router.push('/');
-    }
-  }, []);
-
   return (
     <div className="layout">
       <Header />
       <div className="layout__container">
-        <Navbar {...session.user} />
+        <Navbar />
         <div className="layout__container__content">{children}</div>
       </div>
     </div>
