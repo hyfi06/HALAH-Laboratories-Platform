3c1f09f2e3bdaa74c9bededd7be10646e094eeba
Luis Flores Álvarez <luisfloresdev@gmail.com>
2020-05-21 23:52:08 -0500

    [ADD] Added rol restriction in Users page

--- src/pages/users.jsx
+++ src/pages/users.jsx
@@ -1,27 +1,29 @@
-import PropTypes from 'prop-types';
-import Filter from '../components/Filter';
+import Router from 'next/router';
+import { useEffect } from 'react';
+import { useSession } from '../context/SessionContext';
+// import Filter from '../components/Filter';
 import UsersIcon from '../assets/icons/users.svg';
 import UsersTable from '../components/UsersTable';
 
-function Users({ userType }) {
+function Users() {
+  const { session } = useSession();
+
+  useEffect(() => {
+    if (session && session.user.typeOfUser !== 'admin') {
+      Router.push('/');
+    }
+  }, [session]);
+
   return (
     <main className="users">
       <div className="title">
         <UsersIcon className="title__logo" />
         <h1 className="title__text">Users</h1>
       </div>
-      <Filter typeOfUser={userType} />
+      {/* <Filter typeOfUser={userType} /> */}
       <UsersTable />
     </main>
   );
 }
-
-Users.propTypes = {
-  userType: PropTypes.string,
-};
-
-Users.defaultProps = {
-  userType: '',
-};
 
 export default Users;
