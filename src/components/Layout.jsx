8ce0c54124817e2be53e26116b5e3e729d2a098a
Luis Flores Álvarez <luisfloresdev@gmail.com>
2020-05-21 19:02:22 -0500

    [ADD] Added user state in Navbar component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,33 @@
+import { useState, useEffect } from 'react';
+import Router from 'next/router';
 import PropTypes from 'prop-types';
 import '../assets/styles/components/Layout.scss';
 import Header from './Header';
 import Navbar from './Navbar';
 
 function Layout({ children }) {
+  const [session, setSession] = useState({});
+
+  useEffect(() => {
+    try {
+      setSession(
+        JSON.parse(
+          document.cookie.replace(
+            /(?:(?:^|.*;\s*)session\s*\=\s*([^;]*).*$)|^.*$/,
+            '$1',
+          ),
+        ),
+      );
+    } catch (error) {
+      Router.push('/');
+    }
+  }, []);
+
   return (
     <div className="layout">
       <Header />
       <div className="layout__container">
-        <Navbar />
+        <Navbar {...session.user} />
         <div className="layout__container__content">{children}</div>
       </div>
     </div>
