17a4c47310165101f00535ee8e679c6e8d466629
Luis Flores Álvarez <luisfloresdev@gmail.com>
2020-05-24 13:27:51 -0500

    [UPDATE] Delete onClose prop in Modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,15 +1,13 @@
 import PropTypes from 'prop-types';
 import '../assets/styles/components/Modal.scss';
-import CloseIcon from '../assets/icons/close.svg';
 
-function Modal({ isOpen, onClose, children }) {
+function Modal({ isOpen, children }) {
   if (!isOpen) {
     return null;
   }
 
   return (
     <div className="modal">
-      <CloseIcon className="modal__close" onClick={onClose} />
       <div className="modal__container">{children}</div>
     </div>
   );
@@ -17,7 +15,6 @@ function Modal({ isOpen, onClose, children }) {
 
 Modal.propTypes = {
   isOpen: PropTypes.bool.isRequired,
-  onClose: PropTypes.func.isRequired,
   children: PropTypes.node.isRequired,
 };
 
