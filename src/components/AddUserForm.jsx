fdd384744832d28d7dc067554bd8c645aa6cb6ac
Luis Flores Álvarez <luisfloresdev@gmail.com>
2020-05-24 13:29:22 -0500

    [ADD] Created add-user page

--- src/components/AddUserForm.jsx
+++ src/components/AddUserForm.jsx
@@ -1,91 +1,303 @@
+import { useState } from 'react';
+import Router from 'next/router';
+import axios from 'axios';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { useSession } from '../context/SessionContext';
 import '../assets/styles/components/AddUserForm.scss';
+import ErrorIcon from '../assets/icons/error.svg';
+import SuccessIcon from '../assets/icons/success.svg';
+import Modal from './Modal';
 
 function AddUserForm() {
+  const { session } = useSession();
+  const [prevImg, setPrevImg] = useState(
+    'https://res.cloudinary.com/alexisaraujoapp1/image/upload/v1590279706/alexisapp/ou8drr0g7poddnoqprmb.jpg',
+  );
+  const [errorImg, setErrorImg] = useState(null);
+  const [response, setResponse] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [openModal, setOpenModal] = useState(false);
+  const [modalContent, setModalContent] = useState('');
+
+  async function createUser(userData) {
+    setLoading(true);
+    try {
+      const URL = `${process.env.NEXT_PUBLIC_API_URL}/users`;
+      const config = {
+        headers: { Authorization: `Bearer ${session.token}` },
+      };
+      const res = await axios.post(URL, userData, config);
+      setResponse(res);
+      setModalContent(<AddUserSuccess />);
+    } catch (err) {
+      setError(err);
+      setModalContent(<AddUserError />);
+    }
+    setLoading(false);
+    setOpenModal(true);
+  }
+
+  async function uploadImg() {
+    setErrorImg('Loading image...');
+    const imagedata = document.querySelector('input[type="file"]').files[0];
+    if (imagedata.type.includes('image')) {
+      const formData = new FormData();
+      formData.append('file', imagedata);
+      formData.append('upload_preset', 'alexisapp');
+      const res = await axios.post(
+        'https://api.cloudinary.com/v1_1/alexisaraujoapp1/image/upload',
+        formData,
+      );
+      const { data } = res;
+      setPrevImg(data.secure_url);
+      setErrorImg(null);
+    } else {
+      setErrorImg('The selected file is not an image');
+    }
+  }
+
+  function closeModal() {
+    setOpenModal(false);
+  }
+
+  function AddUserSuccess() {
+    return (
+      <div className="message">
+        <SuccessIcon className="message__icon--positive" />
+        <strong className="message__text">User added successfully</strong>
+        <button
+          className="btn"
+          type="button"
+          onClick={() => {
+            Router.push('/users');
+          }}
+        >
+          Accept
+        </button>
+      </div>
+    );
+  }
+
+  function AddUserError() {
+    return (
+      <div className="message">
+        <ErrorIcon className="message__icon--negative" />
+        <strong className="message__text">
+          An error occurred while adding the user
+        </strong>
+        <button className="btn" type="button" onClick={closeModal}>
+          Accept
+        </button>
+      </div>
+    );
+  }
+
+  function AvatarForm() {
+    return (
+      <form className="avatar-form">
+        {prevImg ? (
+          <figure className="avatar-form__preview">
+            <img
+              className="avatar-form__preview__img"
+              src={prevImg}
+              alt="Avatar"
+            />
+          </figure>
+        ) : (
+          ''
+        )}
+        <div className="input">
+          <label className="input__label">
+            Profile Image
+            <input
+              className="input__field--img"
+              type="file"
+              onChange={uploadImg}
+            />
+          </label>
+          {errorImg ? <span className="input__error">{errorImg}</span> : ''}
+        </div>
+      </form>
+    );
+  }
+
+  function UserForm() {
+    return (
+      <Formik
+        initialValues={{
+          imageURL: prevImg,
+          typeOfUser: 'Patient',
+          firstName: '',
+          lastName: '',
+          documentID: '',
+          email: '',
+          contactNumber: '',
+        }}
+        validate={(values) => {
+          const errors = {};
+          if (!values.typeOfUser) {
+            errors.typeOfUser = 'Please choose a type';
+          }
+          if (!values.firstName) {
+            errors.firstName = 'Please enter a first name';
+          }
+          if (!values.lastName) {
+            errors.lastName = 'Please enter a last name';
+          }
+          if (!values.documentID) {
+            errors.documentID = 'Please enter a document id';
+          }
+          if (!values.email) {
+            errors.email = 'Please enter a email';
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = 'Invalid email address';
+          }
+          if (!values.contactNumber) {
+            errors.contactNumber = 'Please enter a phone number';
+          }
+          return errors;
+        }}
+        onSubmit={async (values, { setSubmitting }) => {
+          await createUser(values);
+          setSubmitting(false);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form className="user-add-form">
+            <section className="user-add-form__section">
+              <div className="input">
+                <label className="input__label">
+                  Type
+                  <Field
+                    className="input__field"
+                    as="select"
+                    name="typeOfUser"
+                    disabled={isSubmitting}
+                  >
+                    <option disabled="disabled">Choose a type</option>
+                    <option value="Patient">Patient</option>
+                    <option value="Doctor">Doctor</option>
+                    <option value="Bacteriologist">Bacteriologist</option>
+                    <option value="Administrator">Administrator</option>
+                  </Field>
+                </label>
+                <ErrorMessage
+                  name="typeOfUser"
+                  component="span"
+                  className="input__error"
+                />
+              </div>
+            </section>
+            <h2 className="user-add-form__title">User Information</h2>
+            <section className="user-add-form__section">
+              <div className="input">
+                <label className="input__label">
+                  First Name
+                  <Field
+                    className="input__field"
+                    type="text"
+                    name="firstName"
+                    disabled={isSubmitting}
+                  />
+                </label>
+                <ErrorMessage
+                  name="firstName"
+                  component="span"
+                  className="input__error"
+                />
+              </div>
+              <div className="input">
+                <label className="input__label">
+                  Last Name
+                  <Field
+                    className="input__field"
+                    type="text"
+                    name="lastName"
+                    disabled={isSubmitting}
+                  />
+                </label>
+                <ErrorMessage
+                  name="lastName"
+                  component="span"
+                  className="input__error"
+                />
+              </div>
+              <div className="input">
+                <label className="input__label">
+                  Document ID
+                  <Field
+                    className="input__field"
+                    type="number"
+                    name="documentID"
+                    disabled={isSubmitting}
+                  />
+                </label>
+                <ErrorMessage
+                  name="documentID"
+                  component="span"
+                  className="input__error"
+                />
+              </div>
+            </section>
+            <h2 className="user-add-form__title">Contact</h2>
+            <section className="user-add-form__section">
+              <div className="input">
+                <label className="input__label">
+                  email
+                  <Field
+                    className="input__field"
+                    type="email"
+                    name="email"
+                    disabled={isSubmitting}
+                  />
+                </label>
+                <ErrorMessage
+                  name="email"
+                  component="span"
+                  className="input__error"
+                />
+              </div>
+              <div className="input">
+                <label className="input__label">
+                  Phone Number
+                  <Field
+                    className="input__field"
+                    type="number"
+                    name="contactNumber"
+                    disabled={isSubmitting}
+                  />
+                </label>
+                <ErrorMessage
+                  name="contactNumber"
+                  component="span"
+                  className="input__error"
+                />
+              </div>
+            </section>
+            <button className="btn" type="submit" disabled={isSubmitting}>
+              Add User
+            </button>
+            {loading ? (
+              <span className="user-add-form__load">Creating user...</span>
+            ) : (
+              ''
+            )}
+          </Form>
+        )}
+      </Formik>
+    );
+  }
+
   return (
     <>
-      <div className='add-user'>
-        <div className='add-user__type input'>
-          <strong className='add-user__type__text '>Type</strong>
-          <select
-            className='add-user__type__select input__field'
-            placeholder='Enter '
-          >
-            <option disabled selected>
-              Select
-            </option>
-            <option value='doctor'>Doctor</option>
-            <option value='patient'>Patient</option>
-            <option value='bacteriologist'>Bacteriologist</option>
-            <option value='admin'>Administrator</option>
-          </select>
-        </div>
-        <h2 className='add-user__title-section'>User Information</h2>
-        <div className='add-user__information-container'>
-          <div className='add-user__input input '>
-            <strong className='add-user__input__strong input__label'>
-              Profile Image
-            </strong>
-            <input
-              placeholder='Upload'
-              type='file'
-              className='add-user__input__field  input__field'
-            ></input>
-          </div>
-          <div className='add-user__input input '>
-            <strong className='add-user__input__strong input__label'>
-              Document ID
-            </strong>
-            <input
-              type='text'
-              className='add-user__input__field input__field'
-            ></input>
-          </div>
-          <div className='add-user__input input '>
-            <strong className='add-user__input__strong input__label'>
-              Name
-            </strong>
-            <input
-              type='text'
-              className='add-user__input__field input__field'
-            ></input>
-          </div>
-          <div className='add-user__input input '>
-            <strong className='add-user__input__strong input__label'>
-              Last Name
-            </strong>
-            <input
-              type='text'
-              className='add-user__input__field input__field'
-            ></input>
-          </div>
-        </div>
-        <h2 className='add-user__title-section'>Contact</h2>
-        <div className='add-user__information-container'>
-          <div className='add-user__input input '>
-            <strong className='add-user__input__strong input__label'>
-              email
-            </strong>
-            <input
-              type='email'
-              className='add-user__input__field input__field'
-            ></input>
-          </div>
-          <div className='add-user__input input '>
-            <strong className='add-user__input__strong input__label'>
-              Phone Number
-            </strong>
-            <input
-              type='tel'
-              className='add-user__input__field input__field'
-            ></input>
-          </div>
-        </div>
-        <div className='add-user__confirm'>
-          <button className='add-user__confirm__button btn ' type='submit'>
-            Add User
-          </button>
-        </div>
-      </div>
+      <AvatarForm />
+      <UserForm />
+      <Modal isOpen={openModal} onClose={closeModal}>
+        {modalContent}
+      </Modal>
     </>
   );
 }
