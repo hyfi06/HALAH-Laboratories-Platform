acd936522b219cbc93362cbea082bb30f952c4ce
Humberto Lopez <humbertolopez954@gmail.com>
2020-05-28 01:37:31 -0500

    [Fix] Fix name of tests

--- src/__test__/components/UserRecord.test.js
+++ src/__test__/components/UserRecord.test.js
@@ -2,7 +2,7 @@ import UserRecord from '../../components/UserRecord';
 import { create } from 'react-test-renderer';
 import { SessionProvider } from '../../context/SessionContext';
 describe('UserRecord Component', () => {
-  test('Should render Login component', () => {
+  test('Should render UserRecord component', () => {
     const component = create(
       <SessionProvider>
         <UserRecord user={{}} />
