a60a523d6efaec222226194d62ae482945f1cd24
alberthor47 <albertocamarena.77t@gmail.com>
2020-05-28 13:14:11 -0500

    [FEATURE] ready the implemetation for adding new test to patients

--- src/components/AddTestForm.jsx
+++ src/components/AddTestForm.jsx
@@ -6,20 +6,16 @@ import useRequest from '../hooks/useRequest';
 import FileIcon from '../assets/icons/file.svg';
 import '../assets/styles/components/AddTestForm.scss';
 
-function AddTestForm({ userData }) {
+function AddTestForm({ userID }) {
   const { session } = useSession();
-  const { response, loading, error } = useRequest(
-    session.token,
-    `${process.env.NEXT_PUBLIC_API_URL}/exams/`,
-    1,
-  );
 
   async function addTest(examId) {
     const postData = {
-      patientId: userData._id,
-      doctorId: session.user._id,
-      examTypeId: examId,
+      patientId: userID,
+      doctorId: session.user.id,
+      examTypeId: examId.exam,
     };
+    console.log(postData);
     try {
       const URL = `${process.env.NEXT_PUBLIC_API_URL}/orders`;
       const config = {
@@ -32,76 +28,133 @@ function AddTestForm({ userData }) {
     }
   }
 
+
+  function getUserData(patientId) {
+    const { response, loading, error } = useRequest(
+      session.token,
+      `${process.env.NEXT_PUBLIC_API_URL}/users/${patientId}`,
+      1,
+    );
+    if (error) {
+      return <h3>{error.message}</h3>;
+    }
+    return (
+      <div className="add-test-form__user-info">
+        { !loading && response.data ? (
+          <>
+            <img alt="user whose new test is filling" src={response.data.imageURL} />
+            <strong>
+              {response.data.firstName}
+              <br />
+              {response.data.lastName}
+            </strong>
+          </>
+        ) : (<div className="loader" />)}
+      </div>
+    );
+  }
+
+  function getExams() {
+    const { response, loading, error } = useRequest(
+      session.token,
+      `${process.env.NEXT_PUBLIC_API_URL}/exams/`,
+      1,
+    );
+    if (error) {
+      return <h3>{error.message}</h3>;
+    }
+    return (
+      <>
+        <div className="add-test-form__form__container">
+          <Formik
+            initialValues={{ examId: '', examName: '' }}
+            validate={(values) => {
+              const errors = {};
+              if (!values.exam) {
+                errors.exam = 'Required: please select an exam';
+              }
+              return errors;
+            }}
+            onSubmit={async (values, { setSubmitting }) => {
+              addTest(values);
+              console.log(values);
+              setSubmitting(false);
+            }}
+          >
+            {({ isSubmitting }) => (
+              <>
+                { !loading && response.data ? (
+                  <Form className="add-test-form__form lg">
+                    <div className="input lg">
+                      <label className="input__label lg">
+                        Select an test by their ID:
+                        <Field
+                          as="select"
+                          className="input__field lg"
+                          type="exam"
+                          name="exam"
+                          disabled={isSubmitting}
+                        >
+                          {response.data.map((exam) => <option>{exam._id}</option>)}
+                        </Field>
+                      </label>
+                      <ErrorMessage
+                        name="exam"
+                        component="div"
+                        className="input__error"
+                      />
+                    </div>
+                    <button className="btn lg" type="submit" disabled={isSubmitting}>
+                      Add new test
+                    </button>
+                  </Form>
+                ) : (<div className="loader" />)}
+              </>
+            )}
+          </Formik>
+        </div>
+        <table className="table container__test">
+          <thead className="table__head">
+            <tr className="table__head__row">
+              <th className="table__head__row__cell colum__test">
+                <strong>Test ID</strong>
+              </th>
+              <th className="table__head__row__cell colum__test">
+                <strong>Test Name</strong>
+              </th>
+            </tr>
+          </thead>
+          <tbody className="table__body">
+            {response.data.map((exam) => (
+              <tr className="table__body__row">
+                <td className="table__body__row__cell">
+                  <p>{exam._id}</p>
+                </td>
+                <td className="table__body__row__cell">
+                  <p>{exam.name}</p>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </>
+    );
+  }
+
   return (
     <main className="add-test-form">
       <div className="add-test-form__title">
         <FileIcon className="add-test-form__title__icon" />
-        <h2 className="add-test-form__title__text">Assign a new exam</h2>
+        <h2 className="add-test-form__title__text">Assign a new test to:</h2>
       </div>
-      <div className="add-test-form__user-info">
-        <img alt="user whose new test is filling" src={userData.imageURL} />
-        <strong>
-          {userData.name}
-          <br />
-          {userData.lastName}
-        </strong>
-      </div>
-      <div className="add-test-form__form__container">
-        <Formik
-          initialValues={{ exam: '' }}
-          validate={(values) => {
-            const errors = {};
-            if (!values.exam) {
-              errors.exam = 'Required: please select an exam';
-            }
-            return errors;
-          }}
-          onSubmit={async (values, { setSubmitting }) => {
-            addTest(values)
-            setSubmitting(false);
-          }}
-        >
-          {({ isSubmitting }) => (
-            <>
-              { !loading && response.data ? (
-                <Form className="add-test-form__form lg">
-                  <div className="input lg">
-                    <label className="input__label lg">
-                      Select an exam
-                      <Field
-                        as="select"
-                        className="input__field lg"
-                        type="exam"
-                        name="exam"
-                        disabled={isSubmitting}
-                      >
-                        {response.data.map((exam) => {
-                          console.log(exam.name);
-                          return <option>{exam._id}</option>;
-                        })}
-                      </Field>
-                    </label>
-                    <ErrorMessage
-                      name="exam"
-                      component="div"
-                      className="input__error"
-                    />
-                  </div>
-                  <button className="btn lg" type="submit" disabled={isSubmitting}>
-                    Add new test
-                  </button>
-                </Form>
-              ) : (<div className="loader" />)}
-            </>
-          )}
-        </Formik>
-      </div>
+      {getUserData(userID)}
+      {getExams()}
     </main>
   );
 }
 
 AddTestForm.propTypes = {
-  userData: PropTypes.object.isRequired,
+  userID: PropTypes.string.isRequired,
 };
 
 export default AddTestForm;
--- src/pages/add-test/[userID].jsx
+++ src/pages/add-test/[userID].jsx
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { useSession } from '../../context/SessionContext';
-import useRequest from '../../hooks/useRequest';
 import '../../assets/styles/pages/EditUser.scss';
 import TestIcon from '../../assets/icons/file.svg';
 import AddTestForm from '../../components/AddTestForm';
@@ -11,25 +10,18 @@ function AddTest() {
   const { userID } = router.query;
   const { session } = useSession();
 
-  const { response, loading, error } = useRequest(
-    session.token,
-    `${process.env.NEXT_PUBLIC_API_URL}/user/${userID}`,
-    1,
-  );
-
   return (
     <>
       <Head>
         <title>HALAH Laboratories: Add Test</title>
       </Head>
-
       <main>
         <div className="title">
           <TestIcon className="title__logo" />
           <h1 className="title__text">Add Test</h1>
         </div>
         <div className="add-test__content">
-          {loading ? <AddTestForm userData={response.data} /> : <div className="loader" />}
+          {session ? <AddTestForm userID={userID} /> : <div className="loader" />}
         </div>
       </main>
     </>
