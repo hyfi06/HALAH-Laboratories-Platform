c4df870149ceab20356a9c9140c7f403c987111e
Humberto Lopez <humbertolopez954@gmail.com>
2020-05-29 01:09:03 -0500

    Add complete snapshots tests

--- src/__test__/components/TestRecord.test.js
+++ src/__test__/components/TestRecord.test.js
@@ -2,10 +2,20 @@ import TestRecord from '../../components/TestRecord';
 import { create } from 'react-test-renderer';
 import { SessionProvider } from '../../context/SessionContext';
 describe('TestRecord Component', () => {
+  const mock = {
+    _id: '5ecda8f2e67da2531795ceb6',
+    name: '1,25-Dihydroxyvitamin D, Serum',
+    shortName: 'DHVD',
+    isComplete: true,
+    appointmentDate: '2020-05-27T23:40:34.096Z',
+    createdAt: '2020-05-26T23:40:34.096Z',
+    resultDate: '2020-05-26T23:41:15.057Z',
+    resultId: '5ecda91b615c1553c4ef1bb8'
+  };
   test('Should render TestRecord component', () => {
     const component = create(
       <SessionProvider>
-        <TestRecord />
+        <TestRecord test={mock} />
       </SessionProvider>
     );
     expect(component.toJSON()).toMatchSnapshot();
